c86ed15db48c0267fd383cdeb8a9953b78c05b14
Atraxi <jdog-jdog@hotmail.com>
2015-01-01 11:47:52 +1300

    Typo for build numbers in Reference.java

--- src/main/java/eviltalkingpie/pandemicraft/reference/Reference.java
+++ src/main/java/eviltalkingpie/pandemicraft/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MOD_ID             = "PandemiCraft";
     public static final String MOD_NAME           = "PandemiCraft";
-    public static final String VERSION            = "@VERSION";
+    public static final String VERSION            = "@VERSION@";
     public static final String CLIENT_PROXY_CLASS = "eviltalkingpie.pandemicraft.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "eviltalkingpie.pandemicraft.proxy.ServerProxy";
     public static final String GUI_FACTORY_CLASS  = "eviltalkingpie.pandemicraft.client.gui.GuiFactory";
